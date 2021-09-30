997345dc4c3952f2527258780e416ae010c18059
Oblivious <me262thanos@gmail.com>
2021-09-30 22:50:17 +0300

    Use singleton instead of full fledged object.

--- src/bank-account/app/login/Login.rb
+++ src/bank-account/app/login/Login.rb
@@ -1,12 +1,12 @@
 class Login < ILogin
     private attr_reader :validator;
 
-    def initialize(validator = Validator.new)
+    def initialize(validator = LoginValidator)
         @validator = validator;
     end
 
     def login(username:, pin:, password:)
-        if validator.validate_input(username, pin, password)
+        if validator.validate(username, pin, password)
             [:ok, "Log In"];
         else
             [:validation_error, "Log In"];
