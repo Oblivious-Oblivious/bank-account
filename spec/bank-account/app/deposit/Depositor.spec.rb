e6e27781b0b3e036a671ba99c4c97228f6776a37
Oblivious <me262thanos@gmail.com>
2021-09-30 22:56:32 +0300

    Implemented a basic version of a Depositor.

--- spec/bank-account/app/deposit/Depositor.spec.rb
+++ spec/bank-account/app/deposit/Depositor.spec.rb
@@ -1,5 +1,36 @@
+def setup_mock_db_data
+    d = UserSaverGateway.new;
+    d.reset_database;
+    oblivious = User.new("oblivious");
+
+    d.store(oblivious, "4242", "random42");
+    d.add(oblivious, 12);
+
+    d;
+end
+
 describe Depositor do
     it "responds to the :deposit message" do
         expect(Depositor.new).to respond_to(:deposit);
+    end
+
+    it "deposits a valid amount of $42 to a new user" do
+        d = setup_mock_db_data;
+        oblivious = User.new("oblivious");
+        
+        dep = Depositor.new;
+        dep.deposit(amount_of: 42, to_user: oblivious);
+
+        expect(d.load_all_users["users"]["oblivious"].balance).to eq(54);
+    end
+
+    it "deposits an invalid amount of $10000 to a new user" do
+        d = setup_mock_db_data;
+        oblivious = User.new("oblivious");
+        
+        dep = Depositor.new;
+        dep.deposit(amount_of: 10_000, to_user: oblivious);
+
+        expect(d.load_all_users["users"]["oblivious"].balance).to eq(12);
     end
 end
--- src/bank-account/app/deposit/Depositor.rb
+++ src/bank-account/app/deposit/Depositor.rb
@@ -1,6 +1,19 @@
 require_relative "IDepositor";
 
 class Depositor < IDepositor
+    private attr_reader :validator;
+
+    def initialize(validator = LargeAmountValidator)
+        @validator = validator;
+    end
+
     def deposit(amount_of:, to_user:)
+        if validator.validate(amount_of)
+            d = UserSaverGateway.new;
+            d.add(to_user, amount_of);
+            [:ok, "Deposit"];
+        else
+            [:large_amount_error, "Deposit"];
+        end
     end
 end
