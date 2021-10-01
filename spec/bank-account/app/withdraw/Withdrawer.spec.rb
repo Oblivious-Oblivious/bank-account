dc099c26a8d238cea87ece83b073ed2937e87e88
Oblivious <me262thanos@gmail.com>
2021-10-02 02:06:31 +0300

    Fixed the Withdrawer specs.

--- spec/bank-account/app/withdraw/Withdrawer.spec.rb
+++ spec/bank-account/app/withdraw/Withdrawer.spec.rb
@@ -1,21 +1,21 @@
-def setup_mock_db_data
-    d = UserSaverGateway.new;
-    d.reset_database;
-    oblivious = User.new("oblivious");
-
-    d.store(oblivious, "4242", "random42");
-    d.add(oblivious, 13);
-
-    d;
-end
-
 describe Withdrawer do
+    before :each do
+        d = UserSaverGateway.new;
+        d.reset_database;
+        oblivious = User.new("oblivious");
+
+        d.store(oblivious, "4242", "random42");
+        d.add(oblivious, 13);
+
+        d;
+    end
+
     it "responds to the :withdraw message" do
         expect(Withdrawer.new).to respond_to(:withdraw);
     end
 
     it "tries to deposit an invalid amount" do
-        d = setup_mock_db_data;
+        d = UserSaverGateway.new;
         oblivious = User.new("oblivious");
 
         w = Withdrawer.new;
@@ -23,5 +23,16 @@ describe Withdrawer do
 
         expect(response).to eq [:validation_error, "Withdraw"];
         expect(d.load(oblivious).balance).to eq 13;
+    end
+
+    it "withdraws a valid amount" do
+        d = UserSaverGateway.new;
+        oblivious = User.new("oblivious");
+
+        w = Withdrawer.new;
+        response = w.withdraw(amount_of: 8, from_user: oblivious);
+
+        expect(response).to eq [:ok, "Withdraw"];
+        expect(d.load(oblivious).balance).to eq 5;
     end
 end
